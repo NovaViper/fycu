83fa90aaf83d860183e16bbc74c7c574500a5442
NovaViper <coder.nova99@mailbox.org>
2025-09-27 19:12:50 -0500

    indev function test take 2

--- conf.d/fish-you-could-use.fish
+++ conf.d/fish-you-could-use.fish
@@ -10,27 +10,24 @@ set --global FYCU_MESSAGE_POSITION before
 # Turned off by default due to bugs with detection
 set --global FYCU_ENABLE_ABBR false
 
-# Setup custom bindings needed to track abbr expansions
-function _fycu_keybindings
-    for mode in default insert
-        bind -M $mode space _fycu_abbr_bind_space
-        bind -M $mode shift-enter _fycu_abbr_bind_newline
-
-        # If oh-my-posh is installed then add custom overlay that appends the
-        # fycu logic and then calls the original oh-my-posh function, otherwise,
-        # just add fycu function
-        if bind enter --user 2>/dev/null | string match -qe _omp_enter_key_handler
-            bind -M $mode enter _omp_enter_key_handler_fycu
-        else
-            bind -M $mode enter _fycu_abbr_bind_newline
-        end
-    end
-end
-
 # Lock in event handlers for keybinds and initialization
 functions -q \
     _fycu_abbr_bind_space \
     _fycu_abbr_bind_newline \
     _omp_enter_key_handler_fycu \
-    _fycu_verify_ignored \
-    _fycu_keybindings
+    _fycu_verify_ignored
+
+# Setup custom bindings needed to track abbr expansions
+for mode in default insert
+    bind -M $mode space _fycu_abbr_bind_space
+    bind -M $mode shift-enter _fycu_abbr_bind_newline
+
+    # If oh-my-posh is installed then add custom overlay that appends the
+    # fycu logic and then calls the original oh-my-posh function, otherwise,
+    # just add fycu function
+    if bind enter --user 2>/dev/null | string match -qe _omp_enter_key_handler
+        bind -M $mode enter _omp_enter_key_handler_fycu
+    else
+        bind -M $mode enter _fycu_abbr_bind_newline
+    end
+end
